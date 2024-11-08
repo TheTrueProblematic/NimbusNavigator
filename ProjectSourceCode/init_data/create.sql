71b66278cd66651190c679c731fc1acde3e83cd8
Logan Moser <lomo8349@colorado.edu>
2024-11-08 11:49:35 -0700

    create.sql update

--- ProjectSourceCode/init_data/create.sql
+++ ProjectSourceCode/init_data/create.sql
@@ -1,4 +1,6 @@
 CREATE TABLE users (
                        username VARCHAR(50) PRIMARY KEY,
-                       password CHAR(60) NOT NULL
+                       password CHAR(60) NOT NULL,
+                       zipcode VARCHAR(5) NOT NULL,
+                       name CHAR(50) NOT NULL
 );
